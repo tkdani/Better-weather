b15990daeb0e247407d89c936248c547b6818a43
Takács Dániel <takacsdaniel.inf@gmail.com>
2025-04-15 18:49:39 +0200

    main weather setup

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import MainWeather from "./Components/Main-weather.component";
+
 function App() {
-  return <div className="">Hello world</div>;
+  return (
+    <div className="m-0 p-0 bg-sky-500 min-h-screen">
+      <MainWeather />
+    </div>
+  );
 }
 
 export default App;
---
+++ weather-app/src/Components/Main-weather.component.tsx
@@ -0,0 +1,13 @@
+const MainWeather = () => {
+  return (
+    <div className="text-center bg-white p-4 mx-auto w-1/5 flex flex-col items-center">
+      <div className="p-2">18 °C</div>
+      <div>
+        <div className="pt-2">Szombathely</div>
+        <div className="pb-2">Hungary</div>
+      </div>
+    </div>
+  );
+};
+
+export default MainWeather;
