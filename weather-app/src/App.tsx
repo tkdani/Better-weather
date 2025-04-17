7e5245e62d01f031dbf40351900737e18860820a
Takács Dániel <takacsdaniel.inf@gmail.com>
2025-04-17 20:41:05 +0200

    main weather api

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -6,21 +6,29 @@ import MainWeather from "./Components/Main-weather.component";
 
 function App() {
   const apiKey = process.env.REACT_APP_API_KEY;
-  const [currentLocation, setCurrentLocation] = useState([]);
-  const name = "Szombathely";
+  const name: string = "Szombathely";
+  const [currentWeather, setCurrentWeather] = useState([]);
+
   useEffect(() => {
     fetch(
-      `https://api.openweathermap.org/geo/1.0/direct?q  =${name}&appid=${apiKey}`
+      `https://api.openweathermap.org/geo/1.0/direct?q=${name}&appid=${apiKey}`
     )
       .then((Response) => Response.json())
       .then((location) => {
-        setCurrentLocation(location[0]);
+        fetch(
+          `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${location[0]["lat"]}&lon=${location[0]["lon"]}&appid=${apiKey}`
+        )
+          .then((Response) => Response.json())
+          .then((weather) => {
+            setCurrentWeather(weather);
+            console.log(weather);
+          });
       });
-  }, []);
+  }, [apiKey, name]);
 
   return (
-    <div className="p-1 min-h-screen flex justify-center font-sans">
-      <MainWeather location={currentLocation} />
+    <div className="bg-sky-300 p-1 min-h-screen flex justify-center font-sans">
+      <MainWeather weather={currentWeather} />
     </div>
   );
 }
--- weather-app/src/Components/Main-weather.component.tsx
+++ weather-app/src/Components/Main-weather.component.tsx
@@ -1,12 +1,12 @@
-const MainWeather = ({ location }: any) => {
-  const name: string = location["name"];
-  const country: string = location["country"];
-  const lat: string = location["lat"];
-  const lon: string = location["lon"];
+const MainWeather = (props: any) => {
+  const { weather } = props;
+  const name: string = weather.name;
+  const country: string = weather.sys.country;
+  const temp: number = weather.main.temp;
 
   return (
     <div className="w-max h-max text-center p-5 mt-10">
-      <div className="p-2 text-6xl font-medium">18 °C</div>
+      <div className="p-2 text-6xl font-medium">{Math.round(temp)} °C</div>
       <div className="p-2">
         <div className="text-3xl">{name}</div>
         <div className="text-lg font-extralight italic">{country}</div>
